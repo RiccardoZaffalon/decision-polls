bf31e44a9268919b291cd78238dc887509406e39
Riccardo Zaffalon <riccardo.zaffalon@gmail.com>
2026-06-07 14:12:36 +0200

    Upgrade db client and orm

--- src/lib/db.server.js
+++ src/lib/db.server.js
@@ -1,13 +1,10 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
+import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
 
-export const client = createClient({
-  url: TURSO_DATABASE_URL,
-  authToken: TURSO_AUTH_TOKEN,
-});
+export const client = createClient({ url: TURSO_DATABASE_URL, authToken: TURSO_AUTH_TOKEN });
 
-const db = drizzle(client);
+const db = drizzle(client, { disableMigrations: true });
 
 export default db;
